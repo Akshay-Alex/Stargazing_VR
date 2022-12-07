4d6cd9da66492e00289c1460c0c7ab1ce23d358a
Akshay Alex <akshay.alex5@gmail.com>
2022-12-07 13:02:56 +0530

    switched to gameobject for stars

--- Assets/VRWorld/Scripts/StarGenerator.cs
+++ Assets/VRWorld/Scripts/StarGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class StarGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
-    public ParticleSystem particle_system;
+    public GameObject StarPrefab;
     public int maxParticles = 10000;
     public TextAsset starCSV;
     public string[] lines;
@@ -15,12 +15,6 @@ public class StarGenerator : MonoBehaviour
     void Awake()
     {
         starsGenerated = false;
-        ParticleSystem.Burst[] bursts = new ParticleSystem.Burst[1];
-        var main = particle_system.main; main.maxParticles = maxParticles;
-        bursts[0].minCount = (short)maxParticles;
-        bursts[0].maxCount = (short)maxParticles;
-        bursts[0].time = 0.0f;
-        particle_system.emission.SetBursts(bursts, 1);
     }
     void LateUpdate()
     {
@@ -28,17 +22,13 @@ public class StarGenerator : MonoBehaviour
         {
             starsGenerated = true;
             lines = starCSV.text.Split('\n');
-            ParticleSystem.Particle[] particleStars = new ParticleSystem.Particle[maxParticles];
-            particle_system.GetParticles(particleStars);
             for (int i = 0; i < maxParticles; i++)
             {
                 string[] components = lines[i].Split(',');
                 normalizedPosition = new Vector3(float.Parse(components[1]),float.Parse(components[3]),float.Parse(components[2])) * farclipplane;
-                particleStars[i].position = normalizedPosition;
-                particleStars[i].remainingLifetime = Mathf.Infinity;
-                particleStars[i].startColor = (Color.white * (1.0f - ((float.Parse(components[0]) + 1.44f) / 8)));
+                Instantiate(StarPrefab, normalizedPosition, Quaternion.identity);
+                //particleStars[i].startColor = (Color.white * (1.0f - ((float.Parse(components[0]) + 1.44f) / 8)));
         }
-        particle_system.SetParticles(particleStars, maxParticles);
         }
     }
 }
