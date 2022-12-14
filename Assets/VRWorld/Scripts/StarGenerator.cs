bfdf8472c102779c734eef636ec308c27fcdfff7
Akshay Alex <akshay.alex5@gmail.com>
2022-12-14 14:05:38 +0530

    Stars generated with enough intensity

--- Assets/VRWorld/Scripts/StarGenerator.cs
+++ Assets/VRWorld/Scripts/StarGenerator.cs
@@ -8,10 +8,13 @@ public class StarGenerator : MonoBehaviour
     public GameObject StarPrefab;
     public int maxParticles = 10000;
     public TextAsset starCSV;
+    public TextAsset StarData;
+    public string[] DataLines;
     public string[] lines;
     public bool starsGenerated;
     private Vector3 normalizedPosition;
-    public float farclipplane;
+    public float DistanceMultiplier;
+    float r = 1000.0f,mag,ra,dec,x,y,z;
     void Awake()
     {
         starsGenerated = false;
@@ -20,15 +23,32 @@ public class StarGenerator : MonoBehaviour
     {
         if (!starsGenerated)
         {
-            starsGenerated = true;
-            lines = starCSV.text.Split('\n');
-            for (int i = 0; i < maxParticles; i++)
-            {
-                string[] components = lines[i].Split(',');
-                normalizedPosition = new Vector3(float.Parse(components[1]),float.Parse(components[3]),float.Parse(components[2])) * farclipplane;
-                Instantiate(StarPrefab, normalizedPosition, Quaternion.identity);
-                //particleStars[i].startColor = (Color.white * (1.0f - ((float.Parse(components[0]) + 1.44f) / 8)));
+            GenerateStars();
         }
+    }
+    void GenerateStars()
+    {
+        starsGenerated = true;
+        DataLines = StarData.text.Split('\n');
+        for (int i = 0; i < maxParticles; i++)
+        {
+            string[] components = DataLines[i].Split(',');
+            mag = float.Parse(components[0]);
+            ra = float.Parse(components[1]);
+            dec = float.Parse(components[2]);
+            SphericalToCartesian(ra, dec, r, ref x, ref y, ref z);
+            normalizedPosition = new Vector3(x/10, y/10, z/10);
+            var star = Instantiate(StarPrefab, normalizedPosition, Quaternion.identity);
+            star.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.white * (5.0f - mag));
+            star.gameObject.name = "star" + i;
         }
+    }
+    void SphericalToCartesian(float ra, float dec, float r, ref float x, ref float y, ref float z)
+    {   
+        dec = (Mathf.PI / 2) - dec;
+        var rr = r * Mathf.Sin(dec);
+        z = rr * Mathf.Cos(ra);
+        x = rr * Mathf.Sin(ra);
+        y = r * Mathf.Cos(dec);
     }
 }
